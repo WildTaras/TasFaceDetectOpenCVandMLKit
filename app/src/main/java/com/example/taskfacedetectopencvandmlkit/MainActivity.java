10044ddd65d614d252a8f07ec1f5ce49f20430b9
WildTaras <khalep.taras@gmail.com>
2020-01-28 15:18:04 +0200

    Create update for detection with unlocked screen

--- app/src/main/java/com/example/taskfacedetectopencvandmlkit/MainActivity.java
+++ app/src/main/java/com/example/taskfacedetectopencvandmlkit/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.taskfacedetectopencvandmlkit;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.TimingLogger;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -31,7 +32,6 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -42,15 +42,23 @@ import java.nio.ByteBuffer;
 import java.util.List;
 
 import static android.graphics.ImageFormat.NV21;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
-import static org.opencv.core.Core.rotate;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     CameraBridgeViewBase cameraView;
 
-    private static final String TAG = "CAMERA";
+    private static final String TAG = "APP";
+    private int height;
+    private int width;
+    private int rotationForMetadata;
+    private int rotationAngle;
+
+
+    private WindowManager windowManager;
+    private SensorManager sensorManager;
+    private Sensor rvSensor;
+    private SensorEventListener rvSensorListener;
+
 
     Mat mRgba;
     Rect rect;
@@ -71,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     cameraView.setCameraPermissionGranted();
                     cameraView.SetCaptureFormat(NV21); // set
                     cameraView.setMaxFrameSize(640, 480 );
+ //                   cameraView.setCameraIndex(99);
                     cameraView.enableView();
                     break;
                 default:
@@ -80,35 +89,114 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
-        txt1 = findViewById(R.id.txt1);
-        time = new TimeCounter();
+
+        ActivityCompat.requestPermissions(
+                this, new String[]{Manifest.permission.CAMERA},1 );
+
         cameraView = (JavaCameraView)findViewById(R.id.jcvCamera);
         cameraView.setVisibility(SurfaceView.VISIBLE);
         cameraView.setCvCameraViewListener(this);
-        ActivityCompat.requestPermissions(
-                this, new String[]{Manifest.permission.CAMERA},1 );
-        Log.e(TAG, "Orientation " + this.getResources().getConfiguration().orientation);
+
+        txt1 = findViewById(R.id.txt1);
+        time = new TimeCounter();
+        Log.e(TAG, "In OnCreate");
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        rvSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        rvSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(
+                        rotationMatrix, event.values);
+                float[] remappedRotationMatrix = new float[16];
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z,
+                        remappedRotationMatrix);
+                float[] orientations = new float[3];
+                SensorManager.getOrientation(remappedRotationMatrix, orientations);
+                for(int i = 0; i < 3; i++) {
+                    orientations[i] = (float)(Math.toDegrees(orientations[i]));
+                }
+                if(orientations[2] > 85) {
+                    Log.e(TAG, "Right rotate");
+                    rotationAngle = 180;
+                    rotationForMetadata = 2;
+                } else if(orientations[2] < -85) {
+                    Log.e(TAG, "Left rotate");
+                    rotationAngle = 0;
+                    rotationForMetadata = 0;
+                } else if(Math.abs(orientations[2]) < 10 && Math.abs(orientations[2]) > -10 ) {
+                    Log.e(TAG, "without characteristics");
+                    rotationAngle = 90;
+                    rotationForMetadata = 3;
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        sensorManager.registerListener(rvSensorListener, rvSensor, SensorManager.SENSOR_DELAY_NORMAL);
+/*
+        windowManager = (WindowManager) this.getSystemService(WINDOW_SERVICE);
+        Log.e(TAG, "Rotattion: " + windowManager.getDefaultDisplay().getRotation());
+        switch (windowManager.getDefaultDisplay().getRotation()) {
+            case 0:
+                rotationAngle = 90;
+                rotationForMetadata = 3;
+                break;
+            case 1:
+                rotationAngle = 0;
+                rotationForMetadata = 0;
+                break;
+            case 3:
+                rotationAngle = 180;
+                rotationForMetadata = 2;
+                break;
+            default:
+                Log.e(TAG, "Something wrong");
+        }
+*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "In OnStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "In OnRestart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.e(TAG, "In OnResume");
         if(!OpenCVLoader.initDebug()){
             Log.e(TAG, "Smth wrong");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, baseLoaderCallback);
         } else {
             baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.e(TAG, "In onPause");
         if(cameraView != null) {
             cameraView.disableView();
         }
@@ -117,6 +205,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.e(TAG, "In onDestroy");
         if(cameraView != null) {
             cameraView.disableView();
         }
@@ -124,20 +213,20 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        this.width = width;
+        this.height = height;
 
     }
 
     @Override
     public void onCameraViewStopped() {
-        mRgba.release();
+ //       mRgba.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
         time.startTime();
-        final int width = mRgba.width();
-        int height = mRgba.height();
         final int upscaleParam = 4;
         FirebaseVisionFaceDetectorOptions options =
                 new FirebaseVisionFaceDetectorOptions.Builder()
@@ -147,7 +236,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 .setWidth(width/upscaleParam)
                 .setHeight(height/upscaleParam)
                 .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
-                .setRotation(FirebaseVisionImageMetadata.ROTATION_270)
+                .setRotation(rotationForMetadata)
                 .build();
 
         final FirebaseVisionFaceDetector detector =
@@ -159,8 +248,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 height/upscaleParam),0,0,Imgproc.INTER_LINEAR);
 
         // rotate image
+
         Point center = new Point(mRgba.cols()>>1,mRgba.rows()>>1);
-        Mat M = Imgproc.getRotationMatrix2D(center,90,1);
+        Mat M = Imgproc.getRotationMatrix2D(center,rotationAngle,1);
         Imgproc.warpAffine(mRgba, mRgba, M, new Size(width,height));
 
 
@@ -209,9 +299,16 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         if(flag != false) {
             int offsetX = (width - height) >> 1;
             int offsetY = offsetX >> 1;
-            Imgproc.rectangle(mRgba, new Point(rect.left + offsetX, rect.top - offsetX),
-                    new Point(rect.right + offsetX, rect.bottom - offsetY ), new Scalar(255, 120, 120), 2);
-            flag = false;
+            Imgproc.circle(mRgba,new Point(rect.left,rect.top), 5, new Scalar(120, 30, 40), 3);
+             if(rotationForMetadata == 3 ){
+                Imgproc.rectangle(mRgba, new Point(rect.left + offsetX, rect.top - offsetX),
+                        new Point(rect.right + offsetX, rect.bottom - offsetY ), new Scalar(255, 120, 120), 2);
+
+            } else {
+                Imgproc.rectangle(mRgba, new Point(rect.left, rect.top),
+                        new Point(rect.right, rect.bottom + offsetY), new Scalar(255, 120, 120), 2);
+            }
+           flag = false;
         }
         return mRgba;
     }
